af896132d03d08bdba6b71ee3cad7a10f39e6a26
Nhan Cao <nhancv92@gmail.com>
2019-07-02 23:59:45 +0700

    basic gl: https://developer.android.com/training/graphics/opengl/environment.html#java

--- app/src/main/java/com/nhancv/opengl/MainActivity.java
+++ app/src/main/java/com/nhancv/opengl/MainActivity.java
@@ -1,13 +1,21 @@
 package com.nhancv.opengl;
 
+import android.opengl.GLSurfaceView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//https://developer.android.com/training/graphics/opengl/environment.html#java
 public class MainActivity extends AppCompatActivity {
+
+    private MyGLSurfaceView glSurfaceView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        glSurfaceView = new MyGLSurfaceView(this);
+        setContentView(glSurfaceView);
     }
 }
