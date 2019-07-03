610d63fe8a288389293f5781d9f90fe9d84756f9
Nhan Cao <nhancv92@gmail.com>
2019-07-03 13:21:20 +0700

    projection: https://developer.android.com/training/graphics/opengl/projection

--- app/src/main/java/com/nhancv/opengl/MyGLRenderer.java
+++ app/src/main/java/com/nhancv/opengl/MyGLRenderer.java
@@ -2,6 +2,7 @@ package com.nhancv.opengl;
 
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
+import android.opengl.Matrix;
 
 import com.nhancv.opengl.shape.Square;
 import com.nhancv.opengl.shape.Triangle;
@@ -13,6 +14,11 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
 
     private Triangle mTriangle;
     private Square mSquare;
+
+    // vPMatrix is an abbreviation for "Model View Projection Matrix"
+    private final float[] vPMatrix = new float[16];
+    private final float[] projectionMatrix = new float[16];
+    private final float[] viewMatrix = new float[16];
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
@@ -27,12 +33,25 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
         GLES20.glViewport(0, 0, width, height);
+
+        float ratio = (float) width / height;
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1, 1, 3, 7);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        mTriangle.draw();
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(viewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(vPMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
+
+        // Draw shape
+        mTriangle.draw(vPMatrix);
     }
 }
--- app/src/main/java/com/nhancv/opengl/shape/Triangle.java
+++ app/src/main/java/com/nhancv/opengl/shape/Triangle.java
@@ -22,9 +22,15 @@ public class Triangle {
     float color[] = {0.63671875f, 0.76953125f, 0.22265625f, 1.0f};
 
     private final String vertexShaderCode =
-            "attribute vec4 vPosition;" +
+            // This matrix member variable provides a hook to manipulate
+            // the coordinates of the objects that use this vertex shader
+            "uniform mat4 uMVPMatrix;" +
+                    "attribute vec4 vPosition;" +
                     "void main() {" +
-                    "  gl_Position = vPosition;" +
+                    // the matrix must be included as a modifier of gl_Position
+                    // Note that the uMVPMatrix factor *must be first* in order
+                    // for the matrix multiplication product to be correct.
+                    "  gl_Position = uMVPMatrix * vPosition;" +
                     "}";
 
     private final String fragmentShaderCode =
@@ -34,6 +40,8 @@ public class Triangle {
                     "  gl_FragColor = vColor;" +
                     "}";
 
+    // Use to access and set the view transformation
+    private int vPMatrixHandle;
     private final int mProgram;
 
     public Triangle() {
@@ -75,7 +83,7 @@ public class Triangle {
     private final int vertexCount = triangleCoords.length / COORDS_PER_VERTEX;
     private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
 
-    public void draw() {
+    public void draw(float[] mvpMatrix) { // pass in the calculated transformation matrix
         // Add program to OpenGL ES environment
         GLES20.glUseProgram(mProgram);
 
@@ -101,5 +109,14 @@ public class Triangle {
 
         // Disable vertex array
         GLES20.glDisableVertexAttribArray(positionHandle);
+
+        // get handle to shape's transformation matrix
+        vPMatrixHandle = GLES20.glGetUniformLocation(mProgram, "uMVPMatrix");
+
+        // Pass the projection and view transformation to the shader
+        GLES20.glUniformMatrix4fv(vPMatrixHandle, 1, false, mvpMatrix, 0);
+
+        // Draw the triangle
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
     }
 }
